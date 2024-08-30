95e35afb1b3651a72cb58804433dace2ab898865
maha-2904 <mahalakshmi200555@gmail.com>
2024-08-30 12:36:34 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,56 @@
+const pi=3.14159;
+console.log(pi);
+let age=25;
+const name="john";
+var isStudent=true;
+let person={
+    firstName:"john",
+    lastName:"Doe",
+    age:25,
+    isStudent:true
+};
+console.log(name,age,isStudent);
+console.log(person);
+let a=20;
+let b=10;
+let h=5;
+let l=15;
+let w=10;
+let r=25;
+let ar=pi*a**2;
+console.log("area of circle=",ar);
+
+let an=0.5*b*h;
+console.log("area of triangle=",an);
+
+let ab=l*w;
+console.log("area of rectangle=",ab);
+
+let ac=a*a;
+console.log("area of square=",ac);
+
+let ad=a*a*a;
+console.log("area of cube=",ad);
+
+let af=pi*r**2/2;
+console.log("area of semicircle=",af);
+
+let Name="Maha";
+let sub1=90;
+let sub2=80;
+let sub3=85;
+let sub4=95;
+let sub5=77;
+let marks=sub1+sub2+sub3+sub4+sub5;
+let average=(sub1+sub2+sub3+sub4+sub5)/5;
+console.log("STUDENT NAME=",name);
+console.log("TAMIL=",sub1);
+console.log("ENGLISH=",sub2);
+console.log("MATHS=",sub3);
+console.log("SCIENCE=",sub4);
+console.log("SOCIAL=",sub5);
+console.log("TOTAL MARKS:",marks);
+console.log("AVERAGE MARKS=",average);
+
+
+
